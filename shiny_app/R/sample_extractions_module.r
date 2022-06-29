cc7ad5a6831950368ee6cb314daeaf542cc6a8a2
jtmiller <jtmiller@umn.edu>
2022-06-29 14:58:20 -0400

    sdf

--- shiny_app/R/sample_extractions_module.r
+++ shiny_app/R/sample_extractions_module.r
@@ -1,69 +1,64 @@
 ### extractions
-
-
 sample_extractions_module_ui <- function(id) {
-  
+
   ns <- NS(id)
 
   tagList(
-    
-    fluidRow(
-      column(
-        width = 2,
-        actionButton(
-          ns("edit_samples"),
-          "Edit Samples",
-          class = "btn-success",
-          style = "color: #fff;",
-          width = '100%'
-        ),
-        tags$br(),
-        tags$br()
-      )
-    ),  
+
     fluidRow(
       column(
         width = 12,
-        title = "Samples",
-        DTOutput(ns('table')) %>%
-          withSpinner(),
-        tags$br(),
-        tags$br()
+        title = "Samples to add to the database",
+        DTOutput(ns('sample_select'))
       )
     ),
-    fluidRow( 
-      actionButton("browser", "browser")
+    fluidRow(
+      column(
+        width = 12,
+        DTOutput(ns('sample_selected'))
+      )
     ),
-    
-    tags$script("$('#browser').hide();"),
-    ##includeScript(path = "sample_table_module.js"),
-    tags$script(src = "sample_table_module.js"),
-    tags$script(paste0("Database_js('", ns(''), "')"))
+    fluidRow(
+      column(
+        width = 8,
+        fluidRow(
+          box( 
+            title = 'Dates',
+            dateInput(inputId = "Collected", label = "Collected:"),
+            dateInput(inputId = "Extracted", label = "Extracted:"),
+            dateInput(inputId = "Filtered", label = "Filtered:"),
+            dateInput(inputId = "Cut_Half", label = "Cut in Half:")
+          )
+        ),
+        fluidRow(
+          column(width = 8,
+            actionButton("edit_samples","Edit Samples")
+          )
+        )
+      )
+    )
   )
 }
-
-
 
 sample_extractions_module  <- function(id, disp) {
 
 
-  selectedLines <- reactive({
-    req(input$`extractions_module-table_rows_selected`)
-    if (is.null(input$`extractions_module-table_rows_selected`)) {
-      return(NULL)
-    } else {
-      rows_selected <- as.numeric(input$`extractions_module-table_rows_selected`) # we need to prefix dt_table_rows_selected with the ID of the UI function "my_ID" and a hyphen
-    }
-    sample_table_module("extractions_module", disp = display_col_pcr, lines = selectedLines)
-  })
-
+  #selectedLines <- reactive({
+  #  req(input$`extractions_module-table_rows_selected`)
+  #  if (is.null(input$`extractions_module-table_rows_selected`)) {
+  #    return(NULL)
+  #  } else {
+  #    rows_selected <- as.numeric(input$`extractions_module-table_rows_selected`) # we need to prefix dt_table_rows_selected with the ID of the UI function "my_ID" and a hyphen
+  #  }
+  #  sample_table_module("extractions_module", disp = display_col_pcr, lines = selectedLines)
+  #})
+#
 
     #smps <- reactive({
     #      df <- selectedLines()
     #      return(df)   
     #    })     
 
-
+    # button <- button_server("mod1")
     sample_table_module("extractions_module", disp = display_col_pcr, lines = selectedLines)
-
 }

303b7ddb7b6de26ff65dab20dd62343b127c04f0
jtmiller <jtmiller@umn.edu>
2022-06-29 10:54:35 -0400

    sdf

--- shiny_app/R/sample_PCR_module.r
+++ shiny_app/R/sample_PCR_module.r
@@ -7,19 +7,35 @@ sample_PCR_module_ui <- function(id) {
   ns <- NS(id)
 
   tagList(
+
     fluidRow(
-      column(
         width = 12,
-        title = "Samples",
-        DTOutput(ns('table')) %>%
-          withSpinner(),
-        tags$br(),
-        tags$br()
-      )
+        title = "Samples to add to the database",
+        DTOutput(ns('sample_select'))
     ),
-    ##includeScript(path = "sample_table_module.js"),
-    tags$script(src = "sample_table_module.js"),
-    tags$script(paste0("Database_js('", ns(''), "')"))
+    fluidRow(
+
+        width = 12,
+        DTOutput(ns('sample_selected'))
+
+    ),
+    fluidRow(
+
+      box( 
+        dateInput("Extracted", label = "Extracted:"),
+        dateInput("PCR_Step_1", label = "PCR Step 1:"),
+        dateInput("Qbited", label = "Qbited:"),
+        selectInput('Storage',  label = 'Choose Storage', c('Strip Tubes','PCR Plate')),
+        dateInput("Dil_1_10", label = "Straight DNA Diluted 1:10:"),
+        dateInput("HCGS", label = "Undiluted Step 1 PCR given to Jeff with list of samples:"),
+        dateInput("Sequenced", label = "Sequenced:"),
+        fileInput('gels', label = 'Gel Picture Upload', multiple = T, accept='image/*')
+      )
+
+    ),
+    fluidRow(
+        actionButton("edit_samples","Edit Samples")
+    )
   )
 }
 
@@ -27,7 +43,5 @@ sample_PCR_module_ui <- function(id) {
 sample_PCR_module <- function(id, disp) {
 
   sample_table_module("PCR_module", disp = display_col_pcr)
-
   
 }
-

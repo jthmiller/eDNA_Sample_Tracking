a6d1c296c53c2972e5db48f465546045ff5147d0
jtmiller <jtmiller@umn.edu>
2022-06-29 15:48:08 -0400

    sdf

--- shiny_app/R/database_module.r
+++ shiny_app/R/database_module.r
@@ -1,56 +1,5 @@
 ### database simple
 
-sample_table_module_ui <- function(id) {
-  
-  ns <- NS(id)
-
-  tagList(
-    fluidRow(
-      column(
-        width = 2,
-        actionButton(
-          ns("add_sample"),
-          "Add",
-          class = "btn-success",
-          style = "color: #fff;",
-          icon = icon('plus'),
-          width = '100%'
-        ),
-        tags$br(),
-        tags$br()
-      )
-    ),
-    fluidRow(
-      column(
-        width = 12,
-        title = "Samples to add to the database",
-        DTOutput(ns('sample_table')) %>%
-          withSpinner(),
-        tags$br(),
-        tags$br()
-      )
-    ),
-    fluidRow(
-      column(
-        width = 12,
-        title = "Samples",
-        DTOutput(ns('table')) %>%
-          withSpinner(),
-        tags$br(),
-        tags$br()
-      )
-    ),
-    ##includeScript(path = "sample_table_module.js"),
-    tags$script(src = "sample_table_module.js"),
-    tags$script(paste0("Database_js('", ns(''), "')"))
-  )
-}
-
-
-
-
-
-
 #' Sample Table Module Server
 #'
 #' The Server portion of the module for displaying the sample datatable
